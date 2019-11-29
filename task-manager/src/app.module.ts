e47471ef25ff77211aaa8a372b07f85707b73b29
mixassio <mixail.bondarev@gmail.com>
2019-11-29 21:20:09 +0300

    first commit

---
+++ task-manager/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  controllers: [AppController],
+  imports: [UsersModule],
+})
+export class AppModule {}
